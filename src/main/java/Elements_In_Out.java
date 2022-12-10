e3b2df2f9231095843a166e040174bc546b05706
Emin Demet <emin.demet@gmail.com>
2022-12-11 00:42:51 +0300

    11.12.2022

--- src/main/java/Elements_In_Out.java
+++ src/main/java/Elements_In_Out.java
@@ -14,27 +14,29 @@ public class Elements_In_Out {
     public static void main(String[] args) {
         List<Integer> userList = new ArrayList<>();
         Scanner input = new Scanner(System.in);
+        int a=0;
 
         do {
             System.out.println("Please choose your action ==> \n To add  the element please enter :1 " +
                     "\n To remove the element please enter :2\n To update the element please enter:3 \n To quit plese enter:4 ");
             int option = input.nextInt();
+
+
+
             switch (option) {
                 case 1:
                     System.out.println("Please enter integer elements ");
 
                     userList.add(input.nextInt());
+
                     break;
                 case 2:
-                    System.out.println("Please enter the element which one dou yıu want to remove ");
-                    int silEleman = input.nextInt();
+                    System.out.println("Please enter the element which one dou you want to remove :");
                     System.out.println(userList);
-                    for (Integer w : userList) {
-                        if (w.equals(silEleman)) {
-                            userList.remove(w);
-                        }
+                    int silEleman = input.nextInt();
 
-                    }
+                    userList.remove((Integer)silEleman);
+
                     break;
                 case 3:
                     System.out.println("Please enter the element which one dou you want to update");
@@ -43,7 +45,10 @@ public class Elements_In_Out {
                     System.out.println("PLease enter new Integer");
                     int yeniSayi = input.nextInt();
                     userList.set(userList.indexOf(guncellenecekSayi), yeniSayi);
-
+                    break;
+                case 4:
+                    System.out.println(" Yazılımı kullandıgınız ıcın teşkkür ederiz ");
+                    a=1;
                     break;
                 default:
                     System.out.println("Please enter your selection");
@@ -51,7 +56,10 @@ public class Elements_In_Out {
             }
 
             System.out.println(userList);
-            System.out.println("Devam etmek icin bir tusa basınız Cıkmak icin '4' basınız ");
-        } while (input.nextInt()!=4) ;
+            if(a==1){
+                break;
+            }
+
+        } while (true) ;
     }
 }
